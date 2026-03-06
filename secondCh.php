ae5ec170a30bb91bd807a92e4e63e976850bc8a0
hassanhmc40-ops <hassanhmc40@gmail.com>
2026-03-06 11:18:12 +0100

    second chalenge

--- secondCh.php
+++ secondCh.php
@@ -1,3 +1,19 @@
 <?php
+$age = 25;
 
+if ($age < 12) {
+    $price = 20;
+} elseif ($age >= 12 && $age <= 18) {
+    $price = 40;
+} elseif ($age > 60) {
+    $price = 30;
+} else {
+    $price = 60;
+}
+
+if ($price == 20) {
+    echo "Special: Children's Menu included! Price: $price DH<br>";
+}
+
+echo "The age is $age, the price is $price DH<br>";
 ?>
