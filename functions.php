c43ecf839b16a1d7eb07b903d8b6c192323705fc
hassanhmc40-ops <hassanhmc40@gmail.com>
2026-03-10 02:17:46 +0100

    submet functions first and second challenges

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+// functions 
+// first challenge 
+echo "first challenge : The Personalized Greeting<br>";
+function greetuser ($name){
+    echo "hellow  $name  ! Ready to code?";
+}
+greetuser ("farid");
+echo "<br>";
+echo "----------------------------------------<br>";
+
+
+echo "second challenge : The Calculation Engine <br>";
+function Calculate_Area($width , $height){
+       $result = $width * $height ;
+       return $result ;
+
+ }
+
+$TotalArea = Calculate_Area( width: 8 ,  height: 12) ;
+echo "The total area is $TotalArea square units";
+echo "<br>";
+echo "----------------------------------------<br>";
+
+echo "Challenge 03: The Logic Gatekeeper"
+
+?>
