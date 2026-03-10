10ad1838084610eb4068360fe839b0886042c3bd
hassanhmc40-ops <hassanhmc40@gmail.com>
2026-03-10 13:56:44 +0100

    complete functions challenges

--- functions.php
+++ functions.php
@@ -3,15 +3,16 @@
 // first challenge 
 echo "first challenge : The Personalized Greeting<br>";
 function greetuser ($name){
-    echo "hellow  $name  ! Ready to code?";
+    echo "Hello  $name  ! Ready to code?";
 }
 greetuser ("farid");
 echo "<br>";
+
 echo "----------------------------------------<br>";
 
-
+// second challenge
 echo "second challenge : The Calculation Engine <br>";
-function Calculate_Area($width , $height){
+function Calculate_Area($width , $height): float|int{
        $result = $width * $height ;
        return $result ;
 
@@ -20,8 +21,52 @@ function Calculate_Area($width , $height){
 $TotalArea = Calculate_Area( width: 8 ,  height: 12) ;
 echo "The total area is $TotalArea square units";
 echo "<br>";
+
 echo "----------------------------------------<br>";
 
-echo "Challenge 03: The Logic Gatekeeper"
+
+// therd challenge : The Logic Gatekeeper
+echo "therd Challenge : The Logic Gatekeeper<br>";
+function is_Adult ($age): bool{
+    if ($age >= 18)
+        {return true;}
+    else{return false;} 
+}
+$age = 12;
+         if (is_Adult($age)) {
+            echo "Access Granted";}
+        else{echo "Access Denied.";}
+        echo "<br>";
+    
+    
+ echo "----------------------------------------<br>";
+ 
+ //fourth challenge : The 'Safe' Multiplier
+ echo "fourth Challenge :  The 'Safe' Multiplier<br>";
+function multiplyNumbers($a, $b){
+    if (is_int($a) && is_int($b)){
+         $result = $a * $b;
+       echo "the result is: " . $result . "<br>";
+    }
+    else { echo "error : invalid input " . "<br>";}
+
+}
+    
+  multiplyNumbers (a: 5, b: 10) . "<br>";
+  multiplyNumbers  (a: 5, b: "apple") ."<br>";
+
+ echo "----------------------------------------<br>";
+ // fifth challenge
+ echo "fifith challenge : The Text Flipper<br>";
+ function manualReverse($text):string{
+ $reversed="";
+    $length =strlen($text);
+    for($i=$length-1;$i>=0;$i--){
+        $reversed .= $text[$i];
+    }
+    return $reversed;
+}
+echo manualReverse("Salam");
+    
 
 ?>
